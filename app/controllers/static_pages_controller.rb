d917bb8233836e48e0d708f19b6170f68019d557
BraianStrak <braianstrak1999@gmail.com>
2020-09-26 16:25:57 +0100

    Completed app

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+    require 'flickr'
+
+    def index
+        if params[:user_id]
+          flickr = Flickr.new
+          @photos = flickr.photos.search(user_id: params[:user_id])
+        end
+    end
+end
